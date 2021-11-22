b5e8678c3acd735bc7d865dbc8e64fd4f282c23a
Thomas Capelle <thomas.capelle@steady-sun.com>
2021-11-22 10:28:49 +0100

    refactor

--- scripts/keras_pets.py
+++ scripts/keras_pets.py
@@ -2,24 +2,14 @@ import os
 import random
 import wandb
 
+import wandb
+from wandb.keras import WandbCallback
+
 from pathlib import Path
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
-
-PROJECT = 'apple_m1_pro'
-GROUP = 'keras'
-JOB_TYPE = 'QuadroT1000'
-
-IMG_SIZE = 128
-BS = 32 
-EPOCHS = 10
-LR = 1e-3
-
 
 # Set the random seeds
 os.environ['TF_CUDNN_DETERMINISTIC'] = '1' 
@@ -28,42 +18,74 @@ np.random.seed(hash("improves reproducibility") % 2**32 - 1)
 tf.random.set_seed(hash("by removing stochasticity") % 2**32 - 1)
 
 
+PROJECT = 'apple_m1_pro'
+GROUP = 'M1Pro'
 
+IMG_SIZE = 128
+BS = 32 
+EPOCHS = 10
+LR = 1e-3
 
-wandb.login()
-run = wandb.init(project=PROJECT, group=GROUP, job_type=JOB_TYPE)
+def train():
+    wandb.login()
+    run = wandb.init(project=PROJECT, group=GROUP)
+    tf.keras.backend.clear_session()
+    model = make_model(input_shape=(IMG_SIZE, IMG_SIZE) + (3,), num_classes=2)
 
-artifact = run.use_artifact('tcapelle/apple_m1_pro/PetImages:latest', type='dataset')
-dataset_dir = artifact.download()
+    train_ds, val_ds = prepare_data()
 
-def get_data(data_dir):
-    print(f'Reading from : {Path(data_dir).absolute()}')
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        Path(data_dir),
-        validation_split=0.2,
-        subset="training",
-        seed=1337,
-        image_size=(IMG_SIZE,IMG_SIZE),
-        batch_size=BS,
+    callbacks = [
+        WandbCallback(save_model=False)
+    ]
+    model.compile(
+        optimizer=keras.optimizers.Adam(LR),
+        loss="binary_crossentropy",
+        metrics=["accuracy"],
     )
-    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        Path(data_dir),
-        validation_split=0.2,
-        subset="validation",
-        seed=1337,
-        image_size=(IMG_SIZE,IMG_SIZE),
-        batch_size=BS,
+    model.fit(
+        train_ds, epochs=EPOCHS, callbacks=callbacks, validation_data=val_ds,
     )
+
+    run.log({'batch_size':BS, 'epochs':EPOCHS, 'image_size':IMG_SIZE, 'lr': LR})
+    run.finish()
+
+def prepare_data():
+    artifact = wandb.use_artifact('tcapelle/apple_m1_pro/PetImages:latest', type='dataset')
+    dataset_dir = artifact.download()
+
+    def get_data(data_dir):
+        print(f'Reading from : {Path(data_dir).absolute()}')
+        train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+            Path(data_dir),
+            validation_split=0.2,
+            subset="training",
+            seed=1337,
+            image_size=(IMG_SIZE,IMG_SIZE),
+            batch_size=BS,
+        )
+        val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+            Path(data_dir),
+            validation_split=0.2,
+            subset="validation",
+            seed=1337,
+            image_size=(IMG_SIZE,IMG_SIZE),
+            batch_size=BS,
+        )
+        return train_ds, val_ds
+
+    train_ds, val_ds = get_data(dataset_dir)
+
+    #speed up dataloading
+    train_ds = train_ds.prefetch(buffer_size=BS)
+    val_ds = val_ds.prefetch(buffer_size=BS)
+
     return train_ds, val_ds
 
-train_ds, val_ds = get_data(dataset_dir)
-
-data_augmentation = lambda x: x
-
-train_ds = train_ds.prefetch(buffer_size=BS)
-val_ds = val_ds.prefetch(buffer_size=BS)
-
 def make_model(input_shape, num_classes):
+    
+    #no data augment for now
+    data_augmentation = lambda x: x
+
     inputs = keras.Input(shape=input_shape)
     # Image augmentation block
     x = data_augmentation(inputs)
@@ -114,21 +136,6 @@ def make_model(input_shape, num_classes):
     outputs = layers.Dense(units, activation=activation)(x)
     return keras.Model(inputs, outputs)
 
-tf.keras.backend.clear_session()
-model = make_model(input_shape=(IMG_SIZE, IMG_SIZE) + (3,), num_classes=2)
 
-from wandb.keras import WandbCallback
-callbacks = [
-    wandb.keras.WandbCallback(save_model=False)
-]
-model.compile(
-    optimizer=keras.optimizers.Adam(LR),
-    loss="binary_crossentropy",
-    metrics=["accuracy"],
-)
-model.fit(
-    train_ds, epochs=EPOCHS, callbacks=callbacks, validation_data=val_ds,
-)
-
-run.log({'batch_size':BS, 'epochs':EPOCHS, 'image_size':IMG_SIZE, 'lr': LR})
-run.finish()
+if __name__=='__main__':
+    train()
