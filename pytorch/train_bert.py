2327be377248dc22d5e1a39f3cbf4fa46f949202
Thomas Capelle <tcapelle@pm.me>
2022-05-19 15:27:01 +0200

    move bert trainer to utils

--- pytorch/train_bert.py
+++ pytorch/train_bert.py
@@ -9,6 +9,8 @@ from torch.optim import AdamW
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, default_data_collator
 from datasets import load_dataset
 
+from utils import MicroTrainer
+
 
 PROJECT = "pytorch-M1Pro"
 ENTITY = "capecape"
@@ -22,7 +24,8 @@ config_defaults = SimpleNamespace(
     dataset="yelp_review_full",
     device="mps",
     gpu_name="M1Pro GPU 16 Cores",
-    num_workers=0
+    num_workers=0,
+    fp16=False,
 )
 
 def parse_args():
@@ -37,6 +40,7 @@ def parse_args():
     parser.add_argument('--gpu_name', type=str, default=config_defaults.gpu_name)
     parser.add_argument('--num_workers', type=int, default=config_defaults.num_workers)
     parser.add_argument('--inference_only', action="store_true")
+    parser.add_argument('--fp16', action="store_true")
     return parser.parse_args()
 
 
@@ -78,70 +82,6 @@ def get_dls(model_name="bert-base-cased", dataset_name="yelp_review_full", batch
 def get_model(model_name="bert-base-cased", num_labels=5):
     return AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
 
-def to_device(batch, device="cpu"):
-    for k,v in batch.items():
-        batch[k] = v.to(device)
-    return batch
-
-@dataclass
-class MicroTrainer:
-    model: torch.nn.Module
-    train_dl: DataLoader
-    test_dl: DataLoader=None
-    device: str="mps"
-
-    def __post_init__(self):
-        self.model = self.model.to(self.device)
-        self.optimizer = AdamW(self.model.parameters(), lr=config_defaults.learning_rate)
-        self.n_steps_per_epoch = math.ceil(len(self.train_dl.dataset) / self.train_dl.batch_size)
-        
-    def do_one_batch(self, batch):
-        batch = to_device(batch, device=self.device)
-        outputs = self.model(**batch)
-        loss = outputs.loss
-        return loss
-
-    def do_one_epoch(self, dl, epoch):
-        for step, batch in enumerate(tqdm(dl, leave=False)):
-            ti = perf_counter()
-            self.optimizer.zero_grad()
-            loss = self.do_one_batch(batch)
-            loss.backward()
-            self.optimizer.step()
-            tf = perf_counter()
-
-
-            self.example_ct += len(batch["labels"])
-            metrics = {"train/train_loss": loss, 
-                        "train/epoch": (step + 1 + (self.n_steps_per_epoch * epoch)) / self.n_steps_per_epoch, 
-                        "train/example_ct": self.example_ct,
-                        "sqe_per_sec":len(batch["labels"])/(tf-ti)}
-            if step + 1 < self.n_steps_per_epoch:
-                # 🐝 Log train metrics to wandb 
-                wandb.log(metrics)
-
-            self.step_ct += 1
-
-    def fit(self, epochs):
-        self.example_ct = 0
-        self.step_ct = 0
-        for epoch in tqdm(range(epochs)):
-            self.model.train()
-            self.do_one_epoch(self.train_dl, epoch)
-    
-    def inference(self, repeat=5):
-        self.model.eval()
-        batch = next(iter(self.train_dl))
-        N = len(batch["labels"])
-        inference_times = []
-        for _ in tqdm(range(repeat)):
-            with torch.inference_mode():
-                ti = perf_counter()
-                _ = self.do_one_batch(batch)
-                tf = perf_counter()
-                inference_times.append(N/(tf-ti))
-        wandb.log({"inference_seq_per_sec": sum(inference_times)/repeat})
-
 def train_bert(config):
     train_dl, _ = get_dls(
         model_name=config.model_name,
@@ -150,7 +90,7 @@ def train_bert(config):
 
     model = get_model(config.model_name).to(config.device)
 
-    trainer = MicroTrainer(model, train_dl, device=config.device)
+    trainer = MicroTrainer(model, train_dl, device=config.device, fp16=config.fp16)
     with wandb.init(project=PROJECT, entity=ENTITY, config=config):
         if not config.inference_only:
             trainer.fit(config.epochs)
---
+++ pytorch/utils.py
@@ -0,0 +1,92 @@
+import math
+from types import SimpleNamespace
+from dataclasses import dataclass
+from time import perf_counter
+
+import wandb
+from tqdm import tqdm
+
+import torch
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+from torch.cuda.amp import autocast
+
+config_defaults = SimpleNamespace(
+    learning_rate=1e-3,
+)
+
+def to_device(batch, device="cpu"):
+    "Move tensors to device"
+    if isinstance(batch, torch.Tensor):
+        batch.to(device)
+    elif isinstance(batch, dict):
+        for k,v in batch.items():
+            batch[k] = v.to(device)
+    else:
+        raise Exception(f"Can't put your batch of type {type(batch)} into device: {device}")
+    return batch
+
+@dataclass
+class MicroTrainer:
+    model: torch.nn.Module
+    train_dl: DataLoader
+    test_dl: DataLoader=None
+    device: str="mps"
+    fp16: bool=False
+
+    def __post_init__(self):
+        self.model = self.model.to(self.device)
+        self.optimizer = AdamW(self.model.parameters(), lr=config_defaults.learning_rate)
+        self.n_steps_per_epoch = math.ceil(len(self.train_dl.dataset) / self.train_dl.batch_size)
+        
+    def do_one_batch(self, batch):
+        batch = to_device(batch, device=self.device)
+        if self.fp16:
+            with torch.cuda.amp.autocast():
+                outputs = self.model(**batch)
+                loss = outputs.loss
+        else:
+            outputs = self.model(**batch)
+            loss = outputs.loss
+        return loss
+
+    def do_one_epoch(self, dl, epoch):
+        for step, batch in enumerate(tqdm(dl, leave=False)):
+            ti = perf_counter()
+            self.optimizer.zero_grad()
+            loss = self.do_one_batch(batch)
+            loss.backward()
+            self.optimizer.step()
+            tf = perf_counter()
+
+
+            self.example_ct += len(batch["labels"])
+            metrics = {"train/train_loss": loss, 
+                        "train/epoch": (step + 1 + (self.n_steps_per_epoch * epoch)) / self.n_steps_per_epoch, 
+                        "train/example_ct": self.example_ct,
+                        "sqe_per_sec":len(batch["labels"])/(tf-ti)}
+            if step + 1 < self.n_steps_per_epoch:
+                # 🐝 Log train metrics to wandb 
+                wandb.log(metrics)
+
+            self.step_ct += 1
+
+    def fit(self, epochs):
+        self.example_ct = 0
+        self.step_ct = 0
+        for epoch in tqdm(range(epochs)):
+            self.model.train()
+            self.do_one_epoch(self.train_dl, epoch)
+    
+    def inference(self, repeat=5):
+        self.model.eval()
+        batch = next(iter(self.train_dl))
+        N = len(batch["labels"])
+        inference_times = []
+        for _ in tqdm(range(repeat)):
+            with torch.inference_mode():
+                ti = perf_counter()
+                _ = self.do_one_batch(batch)
+                tf = perf_counter()
+                inference_times.append(N/(tf-ti))
+        wandb.log({"inference_seq_per_sec": sum(inference_times)/repeat})
